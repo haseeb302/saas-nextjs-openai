3428ebeef94b5dbd158f5441beadf322846216db
Abdul Haseeb <mianhaseeeb302@gmail.com>
2024-05-06 21:21:26 +0500

    prisma issues fix

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,18 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient;
 }
-
-let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+  if (!globalThis.prisma) {
+    globalThis.prisma = new PrismaClient();
   }
-  prisma = global.prisma;
+  prisma = globalThis.prisma;
 }
 
 export default prisma;
