fe49d7e72254a0c00c442b2835cdefd210f2bec5
Abdul Haseeb <mianhaseeeb302@gmail.com>
2024-05-06 19:43:26 +0500

    ui fixes and coming soon added

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,16 @@
 "use client";
 
+import { Badge } from "@/components/ui/badge";
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-import { ArrowRight, Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
+import {
+  ArrowRight,
+  Code,
+  ImageIcon,
+  MessageSquare,
+  Music,
+  VideoIcon,
+} from "lucide-react";
 import { useRouter } from "next/navigation";
 
 const tools = [
@@ -11,40 +19,45 @@ const tools = [
     icon: MessageSquare,
     color: "text-violet-500",
     bgColor: "bg-violet-500/10",
-    href: '/conversation'
+    href: "/conversation",
+    disabled: false,
   },
   {
     label: "Music Generation",
     icon: Music,
     color: "text-emerald-500",
     bgColor: "bg-emerald-500/10",
-    href: '/music'
+    href: "/music",
+    disabled: true,
   },
   {
     label: "Image Generation",
     icon: ImageIcon,
     color: "text-pink-700",
     bgColor: "bg-pink-700/10",
-    href: '/image'
+    href: "/image",
+    disabled: true,
   },
   {
     label: "Video Generation",
     icon: VideoIcon,
     color: "text-orange-700",
     bgColor: "bg-orange-700/10",
-    href: '/video'
+    href: "/video",
+    disabled: true,
   },
   {
     label: "Code Generation",
     icon: Code,
     color: "text-green-700",
     bgColor: "bg-green-700/10",
-    href: '/code'
+    href: "/code",
+    disabled: true,
   },
-]
+];
 
 export default function DashboardPage() {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <div>
       <div className="mb-8 space-y-4">
@@ -54,26 +67,32 @@ export default function DashboardPage() {
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
           Chat with the smartest AI
         </p>
-      </div>   
+      </div>
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
         {tools.map((tool) => (
-          <Card 
-          onClick={() => router.push(tool.href)}
-          key={tool.href}
-          className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          <Card
+            onClick={() => !tool.disabled && router.push(tool.href)}
+            key={tool.href}
+            className={cn(
+              "p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer",
+              tool.disabled && "bg-gray-100 hover:shadow-none cursor-default"
+            )}
           >
             <div className="flex items-center gap-x-4">
               <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
                 <tool.icon className={cn("w-8 h-8", tool.color)} />
               </div>
-              <div className="font-semibold">
-                {tool.label}
-              </div>
+              <div className="font-semibold">{tool.label}</div>
+              {tool?.disabled && (
+                <Badge className="text-[8px]" variant="premium">
+                  Coming Soon!
+                </Badge>
+              )}
             </div>
-            <ArrowRight className="w-5 h-5"/>
+            <ArrowRight className="w-5 h-5" />
           </Card>
         ))}
       </div>
     </div>
-  )
+  );
 }
