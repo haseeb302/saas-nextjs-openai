147386b94dae9787097a0f1ef654ae1803f885a0
Abdul Haseeb <mianhaseeeb302@gmail.com>
2024-05-06 20:38:56 +0500

    primsa fixes

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -4,8 +4,15 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const prismadb = globalThis.prisma || new PrismaClient();
+let prisma: PrismaClient;
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
-export default prismadb;
+export default prisma;
