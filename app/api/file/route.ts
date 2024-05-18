6c329a8dfd790e142fcb2ac206cc3e61daa9742d
Abdul Haseeb <mianhaseeeb302@gmail.com>
2024-05-18 19:20:22 +0500

    file uploads

--- app/api/file/route.ts
+++ app/api/file/route.ts
@@ -36,6 +36,6 @@ export async function GET(req: Request) {
     return NextResponse.json(filePath, { status: 200 });
   } catch (error) {
     console.error("Error occurred:", error);
-    return NextResponse.json("Failed", { status: 400 });
+    return NextResponse.json(error, { status: 400 });
   }
 }
